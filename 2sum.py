e6f204902e7b558490536705444242d414fa3d12
Kharya3 <68401665+Kharya3@users.noreply.github.com>
2020-08-07 13:04:34 +0530

    Update 2sum.py

--- 2sum.py
+++ 2sum.py
@@ -6,11 +6,10 @@ with open("2sum.txt") as file:
         d[int(*a)] = i
         i += 1
 ans = 0
-for t in range(-10000, 10001):
-    print((t+10001)*100/20001, "%")
-    for i in d.keys():
-        j = t - i
-        if j in d:
-            ans += 1
-            break
+t = int(input())
+for i in d.keys():
+    j = t - i
+    if j in d:
+        ans += 1
+        break
 print(ans)
